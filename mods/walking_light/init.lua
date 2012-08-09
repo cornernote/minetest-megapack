c512feab62a9d8db2f9becb95a595ce4da71a8dc
cornernote <cornernote@gmail.com>
2012-08-09 20:40:22 +0930

    updated walking light

--- mods/walking_light/init.lua
+++ mods/walking_light/init.lua
@@ -1,67 +1,109 @@
 local players = {}
-
-minetest.register_on_joinplayer(function(player)
-	table.insert(players, player)
-end)
-
-minetest.register_on_leaveplayer(function(player)
-	table.remove(players, player)
-end)
+local player_positions = {}
+local last_wielded = {}
 
 function round(num) 
         if num >= 0 then return math.floor(num+.5) 
         else return math.ceil(num-.5) end
 end
 
-local player_positions = {}
+minetest.register_on_joinplayer(function(player)
+	table.insert(players, player)
+	
+	local player_name = player:get_player_name()
+	
+	last_wielded[player_name] = player:get_wielded_item():get_name()
+	-- leerer String falls die Fackel auf der ersten Position im Inventar liegt
+	
+	local pos = player:getpos()
+	local rounded_pos = {x=round(pos.x),y=round(pos.y),z=round(pos.z)}
+	
+	player_positions[player_name] = {}
+	player_positions[player_name]["x"] = rounded_pos.x;
+	player_positions[player_name]["y"] = rounded_pos.y;
+	player_positions[player_name]["z"] = rounded_pos.z;
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	table.remove(players, player)
+	
+	local player_name = player:get_player_name()
+	
+	last_wielded[player_name] = nil
+	
+	player_positions[player_name]["x"] = nil
+	player_positions[player_name]["y"] = nil
+	player_positions[player_name]["z"] = nil
+	player_positions[player_name] = nil
+end)
+
+
 
 minetest.register_globalstep(function(dtime)
 	for i,player in ipairs(players) do
-		local wielded = player:get_wielded_item()
-		if wielded:get_name() == "default:torch" then
+		local player_name = player:get_player_name()
+		local wielded_item = player:get_wielded_item():get_name()
+		if wielded_item == "default:torch" then
+			-- Fackel ist in der Hand
 			local pos = player:getpos()
-			local ontop = {x=round(pos.x),y=round(pos.y)+i,z=round(pos.z)}
+			local rounded_pos = {x=round(pos.x),y=round(pos.y)+1,z=round(pos.z)}
 
-			local player_name = player:get_player_name()
-			if player_positions[player_name] == nil then
-				player_positions[player_name] = {}
-				player_positions[player_name]["x"] = ontop.x;
-				player_positions[player_name]["y"] = ontop.y;
-				player_positions[player_name]["z"] = ontop.z;
-			end
-
-			if player_positions[player_name]["x"] ~= ontop.x or player_positions[player_name]["y"] ~= ontop.y or player_positions[player_name]["z"] ~= ontop.z then
-				local is_air  = minetest.env:get_node_or_nil(ontop)
-				-- print('newpos '..minetest.pos_to_string(ontop)..' on '..is_air.name)
-				if is_air ~= nil and (is_air.name == "air" or is_air.name == "walking_light:light")then
-					-- print('set '..minetest.pos_to_string(ontop))
-					if is_air.name == "air" then
-						minetest.env:add_node(ontop,{type="node",name="walking_light:light"})
-					end
-					-- print('remove ('..player_positions[player_name]["x"]..','..player_positions[player_name]["y"]..','..player_positions[player_name]["z"]..')')
-					minetest.env:remove_node({x=player_positions[player_name]["x"], y=player_positions[player_name]["y"], z=player_positions[player_name]["z"]})
-					player_positions[player_name]["x"] = ontop.x
-					player_positions[player_name]["y"] = ontop.y
-					player_positions[player_name]["z"] = ontop.z
+			if last_wielded[player_name] ~= "default:torch" or (player_positions[player_name]["x"] ~= rounded_pos.x or player_positions[player_name]["y"] ~= rounded_pos.y or player_positions[player_name]["z"] ~= rounded_pos.z) then
+				-- Fackel gerade in die Hand genommen oder zu neuem Node bewegt
+				local is_air  = minetest.env:get_node_or_nil(rounded_pos)
+				if is_air ~= nil and is_air.name == "air" then
+					-- wenn an aktueller Position "air" ist, Fackellicht setzen
+					-- print('newpos '..minetest.pos_to_string(rounded_pos)..' on '..is_air.name)
+					minetest.env:add_node(rounded_pos,{type="node",name="walking_light:light"})
+				elseif is_air == nil then
+					-- wenn Spieler beim Start schon Fackel in der Hand hält
+					minetest.env:add_node(rounded_pos,{type="node",name="walking_light:light"})
 				end
+				local old_pos = {x=player_positions[player_name]["x"], y=player_positions[player_name]["y"], z=player_positions[player_name]["z"]}
+				repeat
+					local is_light  = minetest.env:get_node_or_nil(old_pos)
+					if is_light ~= nil and is_light.name == "walking_light:light" and last_wielded[player_name] == "default:torch" then
+						-- print('remove '..minetest.pos_to_string(old_pos))
+						minetest.env:remove_node(old_pos)
+					end
+				until minetest.env:get_node_or_nil(old_pos) ~= "walking_light:light"
+				-- gemerkte Position ist nun die gerundete neue Position
+				player_positions[player_name]["x"] = rounded_pos.x
+				player_positions[player_name]["y"] = rounded_pos.y
+				player_positions[player_name]["z"] = rounded_pos.z
 			end
-		else
+
+			last_wielded[player_name] = "default:torch";
+		elseif last_wielded[player_name] == "default:torch" then
+			-- Fackel nicht in der Hand, aber beim letzten Durchgang war die Fackel noch in der Hand
 			local pos = player:getpos()
-			local ontop = {x=round(pos.x),y=round(pos.y)+i,z=round(pos.z)}
-			local is_air  = minetest.env:get_node_or_nil(ontop)
-			if is_air ~= nil and is_air.name == "walking_light:light" then
-				minetest.env:remove_node(ontop)
-			end
-			if player_positions[player_name] ~= nil then
-				minetest.env:remove_node({x=player_positions[player_name]["x"], y=player_positions[player_name]["y"], z=player_positions[player_name]["z"]})
-			end
+			local rounded_pos = {x=round(pos.x),y=round(pos.y)+1,z=round(pos.z)}
+			repeat
+				local is_light  = minetest.env:get_node_or_nil(rounded_pos)
+				if is_light ~= nil and is_light.name == "walking_light:light" then
+					minetest.env:remove_node(rounded_pos)
+				end
+			until minetest.env:get_node_or_nil(rounded_pos) ~= "walking_light:light"
+
+			local old_pos = {x=player_positions[player_name]["x"], y=player_positions[player_name]["y"], z=player_positions[player_name]["z"]}
+			repeat
+				is_light  = minetest.env:get_node_or_nil(old_pos)
+				if is_light ~= nil and is_light.name == "walking_light:light" then
+					minetest.env:remove_node(old_pos)
+				end
+			until minetest.env:get_node_or_nil(old_pos) ~= "walking_light:light"
+
+			last_wielded[player_name] = wielded_item
 		end
+
+
 	end
 end)
 
 minetest.register_node("walking_light:light", {
 	drawtype = "glasslike",
 	tile_images = {"walking_light.png"},
+	-- tile_images = {"walking_light_debug.png"},
 	inventory_image = minetest.inventorycube("walking_light.png"),
 	paramtype = "light",
 	walkable = false,
